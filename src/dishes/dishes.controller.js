fc7c95ebf739c45de468a3719cf542a31e498cbe
Emrah Isik <emrahisk@gmail.com>
2021-10-04 16:31:04 -0400

    list, name validation

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,143 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+//Check if the name is valid
+ const isNameValid = (req, res, next) => {
+    const { data:{ name } } = req.body;    
+    if( name===undefined ){
+        next({
+            status: 400,
+            message: 'Dish must include a name',
+        })
+    }
+    return true;
+ };
+
+//Check if the description is valid
+const isDescriptionValid = (req, res, next) => {
+    const { data:{ description } } = req.body;    
+    if( !description ){
+        next({
+            status: 400,
+            message: 'Dish must include a description',
+        })
+    }
+    return true;
+ };
+
+//Check if price is valid
+const isPriceValid = (req, res, next) => {
+    const { data:{ price } } = req.body;
+    if( price === undefined ){
+        next({
+            status: 400,
+            message: 'Dish must include a price',
+        })
+    };
+    if( typeof(price) != "number" || price<=0 ){
+        next({
+            status: 400,
+            message: 'Dish must have a price that is an integer greater than 0',
+        })
+    }
+    return true;
+ };
+
+//Check if image url is valid
+const isImageValid = (req, res, next) => {
+    const { data:{ image_url } } = req.body;
+    if( !image_url ){
+        next({
+            status: 400,
+            message: 'Dish must include a image_url',
+        })
+    }
+    return true;
+ };
+
+//Validate new dish entry
+const isValidDish = (req, res, next) => {
+  return (
+    isNameValid(req, res, next) &&
+    isDescriptionValid(req, res, next) &&
+    isPriceValid(req, res, next) &&
+    isImageValid(req, res, next) &&
+    next()
+  );
+};
+
+//Check dish exists
+const dishExists = (req, res, next) => {
+    const { dishId } = req.params;
+    const foundDish = dishes.find(({id}) => id == dishId);
+    if(foundDish){
+        res.locals.dish = foundDish;
+        return next();
+    }else{
+        next({ 
+            status: 404, 
+            message: 'No matching dish found!'
+        });
+    };
+};
+
+//Check if dish id in the req body (if provided) 
+//matches the dishId in the url
+const dishIdMathes = (req, res, next) => {
+    const { dish } = res.locals;    
+    const { dishId } = req.params;
+    if(dish.id){
+        if(dish.id!=dishId){
+            next({
+                status: 400,
+                message: `Dish id does not match route id. Dish: ${dish.id}, Route: ${dishId}`
+            });
+        };
+    };
+    return next();
+}
+
+
+//List all the dishes
+const list = (req, res, next) => res.json({ data: dishes });
+
+
+//Create a new dish entry
+const create = (req, res, next) => {
+    const { data:{ name, description, price, image_url } } = req.body;
+    const newDish = {
+        name,
+        description,
+        price,
+        image_url,
+        id: nextId(),
+    };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish })
+};
+
+//Read a specific dish
+const read = (req, res, next) => {
+    const { dish } = res.locals;
+    res.json({ data: dish });
+};
+
+
+//Update a dish entry
+const update = (req, res, next) => {
+    const { dish } = res.locals;    
+    const { data: update } = req.body;
+    for(let prop in update){
+        dish[prop] = update[prop];
+    }
+    res.json({ data: dish })
+}
+
+
+module.exports = {
+    list,
+    create: [isValidDish, create],
+    read: [dishExists, read],
+    update: [isValidDish, dishExists, dishIdMathes, update]
+}
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,115 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+
+//Check if deliverTo is valid
+const isDeliverToValid = (req, res, next) => {
+    const { data: { deliverTo } } = req.body;
+    if(!deliverTo){
+        next({
+            status: 400,
+            message: 'Order must include a deliverTo',
+        });
+    };
+    return true;
+};
+
+//Check if mobile number is valid
+const isMobileNoValid = (req, res, next) => {
+    const { data: { mobileNumber } } = req.body;
+    if(!mobileNumber){
+        next({
+            status: 400,
+            message: 'Order must include a mobileNumber',
+        });
+    };
+    return true;
+};
+
+//Check if ordered dishes are valid
+const areDishesValid = (req, res, next) => {
+    const { data: { dishes } } = req.body;
+    if(!dishes){
+        next({
+            status: 400,
+            message: 'Order must include a dish',
+        });
+    };
+    if(typeof(dishes) != "object" || !dishes.length){
+        next({
+            status: 400,
+            message: 'Order must include at least one dish',
+        });
+    };
+    return true;
+};
+
+//Check if dish quantitiy is valid
+const isQuantityValid = (req, res, next) => {
+    const { data: { dishes } } = req.body;
+    dishes.forEach(({quantity},index) => {
+        if(!quantity || isNaN(quantity) || quantity<=0){
+            next({
+                status: 400,
+                message:`Dish ${index} must have a quantity that is an integer greater than 0`
+            });
+        };
+    });
+    return true;
+};
+
+// Check if order is valid
+const isOrderValid = (req, res, next) => {
+  return (
+    isDeliverToValid(req, res, next) &&
+    isMobileNoValid(req, res, next) &&
+    areDishesValid(req, res, next) &&
+    isQuantityValid(req, res, next) &&
+    next()
+  );
+};
+
+//Check if order exists
+const orderExists = (req, res, next) => {
+    const { orderId } = req.params;
+    const foundOrder = orders.find(({id}) => id === orderId);
+    if(foundOrder){
+        res.locals.order = foundOrder;
+        return next();
+    };
+    next({
+        status: 404,
+        message: `No matching order found!`,
+    });
+};
+
+
+//List the orders
+const list = (req, res, next) => res.json({ data: orders });
+
+//Create a new order
+const create = (req, res, next) => {
+    const { data: order } = req.body;
+    const newOrder = {
+        id: nextId(), 
+        ...order,
+    };
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder})
+};
+
+//Read a specific order
+const read = (req, res, next) => {
+    const { order } = res.locals;
+    res.json({ data: order });
+};
+
+
+
+
+module.exports = {
+    list,
+    create:[isOrderValid, create],
+    read: [orderExists, read],
+};
